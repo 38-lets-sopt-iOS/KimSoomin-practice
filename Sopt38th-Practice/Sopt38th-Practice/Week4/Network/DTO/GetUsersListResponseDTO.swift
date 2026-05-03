fe030e7819708c4e305460244cdd6cc9e1ee13be
sumchive <haon0319@naver.com>
2026-05-03 18:08:48 +0900

    [network] 유저정보조회 service

---
+++ Sopt38th-Practice/Sopt38th-Practice/Week4/Network/DTO/GetUsersListResponseDTO.swift
@@ -0,0 +1,26 @@
+//
+//  GetUsersListResponseDTO.swift
+//  Sopt38th-Practice
+//
+//  Created by mandoo on 5/2/26.
+//
+
+import Foundation
+
+struct GetUsersListResponseDTO: Decodable {
+    let success: Bool
+    let status: Int
+    let message: String
+    let code: String
+    let data: UsersData
+}
+
+struct UsersData: Decodable {
+    let users: [UserData]
+}
+
+struct UserData: Decodable {
+    let id: Int
+    let name: String
+    let part: String
+}
