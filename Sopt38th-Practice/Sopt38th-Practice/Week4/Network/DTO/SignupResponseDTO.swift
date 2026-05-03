cfc5bef183cd5e976727e6c5126319185c8a08b5
sumchive <haon0319@naver.com>
2026-05-03 18:08:25 +0900

    [network] 회원가입 service

---
+++ Sopt38th-Practice/Sopt38th-Practice/Week4/Network/DTO/SignupResponseDTO.swift
@@ -0,0 +1,21 @@
+//
+//  SignupResponseDTO.swift
+//  Sopt38th-Practice
+//
+//  Created by mandoo on 5/2/26.
+//
+
+import Foundation
+
+struct SignupResponseDTO: Decodable {
+    let success: Bool
+    let status: Int
+    let message: String
+    let code: String
+    let meta: ErrorResponseDTO?
+}
+
+struct ErrorResponseDTO: Decodable {
+    let path: String
+    let timestamp: String
+}
